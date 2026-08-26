36be5bdde33297a538487ebcd4ca18ab31d5e5ff
CraftyCurtis05 <craftycurtis05@gmail.com>
2026-08-26 03:25:11 -0400

    Curate and update portfolio certificates

--- src/data/certificates.js
+++ src/data/certificates.js
@@ -5,188 +5,83 @@
 const certificates = [
   {
     id: 1,
-    image: "01_Tech_Elevator_Certificate.webp",
+    image: "01-tech-elevator.webp",
     title: "Tech Elevator: Certificate of Completion"
   },
   {
     id: 2,
-    image: "02_Learn_HTML.webp",
+    image: "02-html.webp",
     title: "Learn HTML Course Certificate"
   },
   {
     id: 3,
-    image: "03_HTML_Forms.webp",
-    title: "Learn HTML: Forms Course Certificate"
-  },
-  {
-    id: 4,
-    image: "04_HTML_Fundamentals.webp",
-    title: "Learn HTML: Fundamentals Course Certificate"
-  },
-  {
-    id: 5,
-    image: "05_HTML_Semantics.webp",
-    title: "Learn HTML: Semantics Course Certificate"
-  },
-  {
-    id: 6,
-    image: "06_HTML_Tables.webp",
-    title: "Learn HTML: Tables Course Certificate"
-  },
-  {
-    id: 7,
-    image: "07_Learn_CSS.webp",
+    image: "03-css.webp",
     title: "Learn CSS Certificate"
   },
   {
-    id: 8,
-    image: "08_Learn_Intermediate_CSS.webp",
+    id: 4,
+    image: "04-intermediate-css.webp",
     title: "Learn Intermediate CSS Certificate"
   },
   {
-    id: 9,
-    image: "09_CSS_Accessibility.webp",
+    id: 5,
+    image: "05-css-accessibility.webp",
     title: "Learn CSS: Accessibility Course Certificate"
   },
   {
-    id: 10,
-    image: "10_CSS_Box_Model_And_Layout.webp",
-    title: "Learn CSS: Box Model and Layout Course Certificate"
-  },
-  {
-    id: 11,
-    image: "11_CSS_Browser_Compatibility.webp",
-    title: "Learn CSS: Browser Compatibility Course Certificate"
-  },
-  {
-    id: 12,
-    image: "12_CSS_Colors.webp",
-    title: "Learn CSS: Colors Course Certificate"
-  },
-  {
-    id: 13,
-    image: "13_CSS_Flexbox_And_Grid.webp",
-    title: "Learn CSS: Flexbox and Grid Course Certificate"
-  },
-  {
-    id: 14,
-    image: "14_CSS_Introduction.webp",
-    title: "Learn CSS: Introduction Course Certificate"
-  },
-  {
-    id: 15,
-    image: "15_CSS_Responsive_Design.webp",
+    id: 6,
+    image: "06-css-responsive-design.webp",
     title: "Learn CSS: Responsive Design Course Certificate"
   },
   {
-    id: 16,
-    image: "16_CSS_Transitions_And_Animations.webp",
-    title: "Learn CSS: Transitions and Animations Course Certificate"
-  },
-  {
-    id: 17,
-    image: "17_CSS_Typography_And_Fonts.webp",
-    title: "Learn CSS: Typography and Fonts Course Certificate"
-  },
-  {
-    id: 18,
-    image: "18_CSS_Variables_And_Functions.webp",
-    title: "Learn CSS: Variables and Functions Course Certificate"
-  },
-  {
-    id: 19,
-    image: "19_Learn_Color_Design.webp",
-    title: "Learn Color Design Course Certificate"
-  },
-  {
-    id: 20,
-    image: "20_Learn_JavaScript.webp",
+    id: 7,
+    image: "07-javascript.webp",
     title: "Learn JavaScript Certificate"
   },
   {
-    id: 21,
-    image: "21_Learn_Intermediate_JavaScript.webp",
+    id: 8,
+    image: "08-intermediate-javascript.webp",
     title: "Learn Intermediate JavaScript Certificate"
   },
   {
-    id: 22,
-    image: "22_Building_Interactive_JavaScript_Websites.webp",
+    id: 9,
+    image: "09-building-interactive-javascript-websites.webp",
     title: "Building Interactive JavaScript Websites Certificate"
   },
   {
-    id: 23,
-    image: "23_Make_Website_With_NameCheap.webp",
-    title: "How to Make a Website with NameCheap Certificate"
-  },
-  {
-    id: 24,
-    image: "24_Learn_Vuejs.webp",
-    title: "Learn Vue.js Certificate"
-  },
-  {
-    id: 25,
-    image: "25_Learn_Bootstrap.webp",
+    id: 10,
+    image: "10-bootstrap.webp",
     title: "Learn Bootstrap Certificate"
   },
   {
-    id: 26,
-    image: "26_Learn_SQL.webp",
+    id: 11,
+    image: "11-vuejs.webp",
+    title: "Learn Vue.js Certificate"
+  },
+  {
+    id: 12,
+    image: "12-sql.webp",
     title: "Learn SQL Certificate"
   },
   {
-    id: 27,
-    image: "27_Learn_Spring.webp",
+    id: 13,
+    image: "13-spring.webp",
     title: "Learn Spring Certificate"
   },
   {
-    id: 28,
-    image: "28_Spring_Fundamentals_And_Controllers.webp",
-    title: "Learn Spring: Fundamentals and Controllers Course Certificate"
-  },
-  {
-    id: 29,
-    image: "29_Spring_Building_An_App.webp",
-    title: "Learn Spring: Building an App Course Certificate"
-  },
-  {
-    id: 30,
-    image: "30_Create_REST_APIs_With_Spring_And_Java.webp",
+    id: 14,
+    image: "14-rest-apis-with-spring-and-java.webp",
     title: "Create REST APIs with Spring and Java Skill Path Certificate"
   },
   {
-    id: 31,
-    image: "31_Fundamentals_Of_Cybersecurity.webp",
+    id: 15,
+    image: "15-fundamentals-of-cybersecurity.webp",
     title: "Fundamentals of Cybersecurity Skill Path Certificate"
   },
   {
-    id: 32,
-    image: "32_Comptia_Security_Fundamental_Security_Concepts.webp",
-    title: "CompTIA Security+: Fundamental Security Concepts Course Certificate"
-  },
-  {
-    id: 33,
-    image: "33_Comptia_Security_Practical_Cryptography.webp",
-    title: "CompTIA Security+: Practical Cryptography Course Certificate"
-  },
-  {
-    id: 34,
-    image: "34_Comptia_Security_Risk_Management.webp",
+    id: 16,
+    image: "16-comptia-security-risk-management.webp",
     title: "CompTIA Security+: Risk Management Course Certificate"
-  },
-  {
-    id: 35,
-    image: "35_Comptia_Security_Security_Goals_And_Controls.webp",
-    title: "CompTIA Security+: Security Goals and Controls Course Certificate"
-  },
-  {
-    id: 36,
-    image: "36_Comptia_Security_Survey_Of_Malicious_Activities.webp",
-    title: "CompTIA Security+: Survey of Malicious Activities Course Certificate"
-  },
-  {
-    id: 37,
-    image: "37_Comptia_Security_Threat_Actors_And_Vectors.webp",
-    title: "CompTIA Security+: Threat Actors and Vectors Course Certificate"
   }
 ];
 
